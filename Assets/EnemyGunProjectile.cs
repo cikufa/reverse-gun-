3b0f9c907e7d5a6867dd492c2c60f03617c5495f
Shekoufeh Sadeghi <48587790+cikufa@users.noreply.github.com>
2022-12-02 17:18:44 +0330

    init

---
+++ Assets/EnemyGunProjectile.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGunProjectile : MonoBehaviour
+{
+    [SerializeField] float timeToDestroy;
+    internal Vector2 targetPosition;
+
+    void Start()
+    {
+        Destroy(gameObject, timeToDestroy);
+    }
+
+    private void Update()
+    {
+        if (Vector2.SqrMagnitude((Vector2)transform.position - targetPosition) < 0.5f)
+        {
+            gameObject.layer = LayerMask.NameToLayer("Bullet");
+            GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
+            Destroy(gameObject, 0.1f);
+        }
+    }
+}
